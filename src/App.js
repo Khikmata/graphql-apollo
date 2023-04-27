55690c0814c16dfb0af4d1bd53d46895c8f2f745
Kamil <windycate03@gmail.com>
2023-04-27 22:58:21 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import { useMutation, useQuery } from '@apollo/client';
+import React, { useEffect } from 'react';
 import './App.css';
+import { CREATE_USER } from './mutation/user';
+import { GET_ALL_USERS, GET_USER_BY_ID } from './services/user';
 
 function App() {
+  const { data: allUsersData, loading, error, refetch } = useQuery(GET_ALL_USERS)
+  const { data: singleUserData } = useQuery(GET_USER_BY_ID,)
+  const [newUser] = useMutation(CREATE_USER)
+
+  const [users, setUsers] = React.useState([]);
+
+  const [username, setUsername] = React.useState('')
+  const [age, setAge] = React.useState('')
+
+
+  console.log(username, age)
+  useEffect(() => {
+    if (!loading && !error) {
+      setUsers(allUsersData.getAllUsers)
+    }
+  }, [allUsersData])
+
+
+  const addUser = (e) => {
+    e.preventDefault()
+    newUser({
+      variables: {
+        input: {
+          username, age
+        }
+      }
+    }).then(({ data }) => {
+      console.log(data)
+      setUsername('');
+      setAge('');
+    })
+  }
+
+  const getAll = (e) => {
+    e.preventDefault()
+    refetch();
+  }
+
+  if (loading) {
+    return <p>Идет загрузка данных...</p>
+  }
+  if (error) {
+    return <p>Идет загрузка данных...</p>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form>
+        <input placeholder='username' type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input placeholder='age' type='number' value={age} onChange={(e) => setAge(e.target.value)} />
+        <div className='btn'>
+          <button onClick={(e) => addUser(e)}>Создать пользователя</button>
+          <button onClick={(e) => getAll(e)}>Получить пользователей</button>
+        </div>
+      </form>
+      <div>
+        {users.map((user, id) => (
+          <p key={id} className='user' > {user.username}({user.age})</p>
+        ))}
+      </div>
+    </div >
   );
 }
 
